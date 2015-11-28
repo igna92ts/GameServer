9f179322fbf080b5f3092e4bce46f1d3e6fd2e1f
igna92ts <igna_torres92@hotmail.com>
2015-11-27 21:44:56 -0300

    Initial commit

---
+++ models/Example_model.js
@@ -0,0 +1,19 @@
+var mongoose = require('mongoose');
+Schema = mongoose.Schema;
+
+accountSchema = new Schema({
+  PlayerName : { type: String , required: true },
+  Experience : { type: Number, default: 0},
+  Level : { type: Number, default: 1},
+  Stamina : {type: Number, default: 20},
+  SoftCurrency : {type: Number, default: 0},
+  HardCurrency : {type: Number, default: 0},
+  LogDays : {type: Number, default: 1},
+  TotalLogDays : {type: Number, default: 1},
+  FriendPoints : {type: Number, default: 0},
+},{collection: 'Account'}),
+
+
+Account = mongoose.model('Account', accountSchema);
+
+module.exports = Account;
